f5aab3d2676ffecaea0d2b5686aa66efd81dc50b
addykan <addykan@gmail.com>
2021-12-03 12:07:27 -0500

    Finished dec2 part2

--- dec2/dec2.py
+++ dec2/dec2.py
@@ -26,10 +26,38 @@ def parseInstructions(instructions):
     print(f'Depth = {depth}, length = {length}, result = {depth * length}')
 
 
+'''
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+    It increases your horizontal position by X units.
+    It increases your depth by your aim multiplied by X.
+
+'''
+def parseInstructionsPart2(instructions):
+    aim = 0
+    horizontal = 0
+    depth = 0
+    for line in instructions:
+        if line == '':
+            continue
+        direction, number = line.split()
+        number = int(number)
+        match direction:
+            case 'forward':
+                horizontal += number
+                depth += aim * number
+            case 'up':
+                aim -= number
+            case 'down':
+                aim += number
+    print(f'Depth = {depth}, horizontal = {horizontal}, result = {depth * horizontal}')
+
 def main():
     filePath = 'input.txt'
     instructions = readFile(filePath).splitlines()
     parseInstructions(instructions)
+    parseInstructionsPart2(instructions)
     return 0
 
 
